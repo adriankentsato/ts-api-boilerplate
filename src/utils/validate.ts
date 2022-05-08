b8ab2776c14c6c8836be8daf9cb2a2e33120bd6b
Kent Adrian Sato <31067830+adriankentsato@users.noreply.github.com>
2022-05-08 00:28:38 -0800

    Added express-validator

---
+++ src/utils/validate.ts
@@ -0,0 +1,38 @@
+/**
+ * MIT License
+ *
+ * Author: Kent Adrian Sato
+ * Date: May 7, 2023
+ */
+
+import { validationResult } from 'express-validator';
+import { ExpressFunction } from '../interfaces/express';
+import { Handler } from '../interfaces/handler';
+import ErrorResponse from '../errors/error-response';
+
+/** This uses the express-validator to check for the data valididty. */
+export default function validate(mw: ExpressFunction): Handler {
+    return (req) =>
+        new Promise<void>((resolve, reject) => {
+            console.info('[Validation Check] Checking for the validity of your data.');
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            mw(req, {} as any, (error) => {
+                if (error != null) reject(error);
+                else {
+                    const x = validationResult(req);
+
+                    if (!x.isEmpty()) {
+                        const err = x.array()[0];
+
+                        console.error('[Validation Error]', err.msg);
+
+                        reject(new ErrorResponse('BAD REQUEST', 'Bad Request', 400));
+                    } else {
+                        console.info('[Validation Succeeded] Validation okay.');
+
+                        resolve();
+                    }
+                }
+            });
+        });
+}
