4c65e7e36fe5341f20e6158cef5b71edea813a4e
Kent Adrian Sato <31067830+adriankentsato@users.noreply.github.com>
2022-07-29 09:21:06 +0800

    data model base class with auto-mapper and intellisense support

---
+++ src/models/invoice.ts
@@ -0,0 +1,30 @@
+import { IGenericType } from '../interfaces/types/generictype';
+import { IObjectKeysMap } from '../interfaces/types/object-map';
+import CreateModel from '../utils/base-model';
+
+type TInvoice = IObjectKeysMap<['id', 'invoiceDate', 'userId', 'customerId'], string>;
+type TDbInvoice = IObjectKeysMap<['id', 'invDate', 'userId', 'custId'], string>;
+
+const Model = CreateModel<TInvoice, TDbInvoice>({
+    id: 'id',
+    invoiceDate: 'invDate',
+    userId: 'userId',
+    customerId: 'custId',
+});
+
+export default class Invoice extends Model {
+    private customer: IGenericType;
+
+    constructor(dbInv: TDbInvoice) {
+        super(dbInv);
+
+        this.customer = {};
+    }
+
+    public toJSON() {
+        return {
+            ...super.toJSON(),
+            customer: this.customer,
+        };
+    }
+}
---
+++ src/utils/base-model.ts
@@ -0,0 +1,38 @@
+import { IGenericType } from '../interfaces/types/generictype';
+import { IObjectMap } from '../interfaces/types/object-map';
+
+interface IModel<T, R> {
+    new (data: R): T;
+}
+
+interface ISerialize<T> {
+    toJSON(): Readonly<T>;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function CreateModel<T extends Record<string, any>, R extends Record<string, any>>(
+    mapper: Readonly<IObjectMap<string>>,
+) {
+    return class {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        constructor(data: R) {
+            Object.keys(mapper).forEach((key) => {
+                Object.defineProperty(this, key, {
+                    get: () => data[mapper[key]],
+                    enumerable: true,
+                    configurable: true,
+                });
+            });
+        }
+
+        public toJSON() {
+            const ret: IGenericType = {};
+
+            Object.keys(mapper).forEach((key) => {
+                ret[key] = (this as IGenericType)[key];
+            });
+
+            return ret;
+        }
+    } as IModel<Readonly<T> & ISerialize<T>, R>;
+}
