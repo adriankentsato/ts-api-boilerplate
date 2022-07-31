5333128b87083898b156e4321832d1450099ec8b
Kent Adrian Sato <31067830+adriankentsato@users.noreply.github.com>
2022-08-01 00:04:08 +0800

    Added field mapper

--- src/models/invoice.ts
+++ src/models/invoice.ts
@@ -1,23 +1,25 @@
 import { IGenericType } from '../interfaces/types/generictype';
+import { IObjectKeysMap, IObjectMap } from '../interfaces/types/object-map';
 import CreateModel from '../bases/base-model';
 import DefaultClass from '../bases/default-class';
 
 type TInvoice = IObjectKeysMap<['id', 'invoiceDate', 'userId', 'customerId', 'data'], string>;
 type TDbInvoice = IObjectKeysMap<['id', 'invDate', 'userId', 'custId'], string>;
+type TInvoiceMapper = IObjectKeysMap<['customer'], IObjectMap<string>>;
 
-const Model = CreateModel<TInvoice, TDbInvoice>({
+const Model = CreateModel<TInvoice, TDbInvoice, typeof DefaultClass, TInvoiceMapper>({
     id: 'id',
     invoiceDate: 'invDate',
     userId: 'userId',
     customerId: 'custId',
-    data: (data) => data.custId,
+    data: (data, fieldMap) => fieldMap.customer[data.custId.toString()],
 });
 
 export default class Invoice extends Model {
     private customer: IGenericType;
 
-    constructor(dbInv: TDbInvoice) {
-        super(dbInv);
+    constructor(dbInv: TDbInvoice, mapper: TInvoiceMapper) {
+        super(dbInv, mapper);
 
         this.customer = {};
     }
