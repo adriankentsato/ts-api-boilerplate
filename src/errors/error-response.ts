6b2fb9249213ed7ec81bd0052d8c66ec0a55b46d
Kent Adrian Sato <31067830+adriankentsato@users.noreply.github.com>
2023-05-08 00:27:38 +0800

    Added response error throwables

---
+++ src/errors/error-response.ts
@@ -0,0 +1,24 @@
+/**
+ * MIT License
+ *
+ * Author: Kent Adrian Sato
+ * Date: May 7, 2023
+ */
+
+/** */
+import ApiReponse from './api-response';
+
+export default class ErrorResponse extends ApiReponse {
+    public constructor(errCode: string, message: string, statusCode: number = 500) {
+        super(
+            {
+                response: {},
+                error: {
+                    code: errCode,
+                    message,
+                },
+            },
+            statusCode,
+        );
+    }
+}
